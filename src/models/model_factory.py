76c86a5810190c03e081a982a39684c230c4aca0
Dylan Boyle <dylan96.boyle@gmail.com>
2026-03-24 00:18:25 +0000

    adjusted model factory file

--- src/models/model_factory.py
+++ src/models/model_factory.py
@@ -1,14 +1,15 @@
 from src.config import MODEL_NAME
+from src.models.chained_multioutput_model import ChainedMultiOutputModel
 from src.models.random_forest_model import RandomForestModel
 from src.models.logistic_regression_model import LogisticRegressionModel
 from src.models.multinomial_nb_model import MultinomialNBModel
 
 def get_model():
     if MODEL_NAME == "random_forest":
-        return RandomForestModel()
+        return ChainedMultiOutputModel(RandomForestModel)
     elif MODEL_NAME == "logistic_regression":
-        return LogisticRegressionModel()
+        return ChainedMultiOutputModel(LogisticRegressionModel)
     elif MODEL_NAME == "multinomial_nb":
-        return MultinomialNBModel()
+        return ChainedMultiOutputModel(MultinomialNBModel)
     else:
         raise ValueError(f"Unsupported model: {MODEL_NAME}")
