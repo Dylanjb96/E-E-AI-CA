aaae1acac77c8a3df7dcb8318f705d757f982e90
Dylan Boyle <dylan96.boyle@gmail.com>
2026-03-24 00:16:11 +0000

    adjusted lr file

--- src/models/logistic_regression_model.py
+++ src/models/logistic_regression_model.py
@@ -1,20 +1,5 @@
 from sklearn.linear_model import LogisticRegression
-from src.models.base_model import BaseModel
 
-class LogisticRegressionModel(BaseModel):
+class LogisticRegressionModel(LogisticRegression):
     def __init__(self):
-        self.model_t2 = LogisticRegression(max_iter=1000, random_state=42)
-        self.model_t23 = LogisticRegression(max_iter=1000, random_state=42)
-        self.model_t234 = LogisticRegression(max_iter=1000, random_state=42)
-
-    def train(self, data_bundle):
-        self.model_t2.fit(data_bundle.X_train, data_bundle.y_train_t2)
-        self.model_t23.fit(data_bundle.X_train, data_bundle.y_train_t23)
-        self.model_t234.fit(data_bundle.X_train, data_bundle.y_train_t234)
-
-    def predict(self, data_bundle):
-        return {
-            "t2": self.model_t2.predict(data_bundle.X_test),
-            "t23": self.model_t23.predict(data_bundle.X_test),
-            "t234": self.model_t234.predict(data_bundle.X_test),
-        }
+        super().__init__(max_iter=1000, random_state=42)
